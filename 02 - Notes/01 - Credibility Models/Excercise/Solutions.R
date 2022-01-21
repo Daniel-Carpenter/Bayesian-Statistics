244247532bbbb22a4d20b03f73ca91e3f4207c8f
Daniel-Carpenter <dbry1024@gmail.com>
2022-01-21 15:55:58 -0600

    Credibility Models Initial Commit

---
+++ 02 - Notes/01 - Credibility Models/Excercise/Solutions.R
@@ -0,0 +1,62 @@
+# Exercises chapter 2
+
+# 2.1
+# model a
+
+ma = function(x) rep(1/4, length(x))
+
+# model b
+
+mb = function(x) x/10
+
+# model c
+
+mc = function(x) 12/(25*x)
+
+ma(1:4)
+mb(1:4)
+mc(1:4)
+ 
+
+
+x=1:4
+ya=ma(x)
+
+windows()
+plot(ya~x, type = "h", lwd = 10, main = "Model A")
+
+yb = mb(x)
+windows()
+plot(yb~x, type = "h", lwd = 10, main = "Model B")
+
+yc = mc(x)
+windows()
+plot(yc~x, type = "h", lwd = 10, main = "Model C")
+
+sum(ya)
+sum(yb)
+sum(yc)
+
+# Using GGPLOT
+# 
+
+dfa = data.frame(x =x, ya = ya)
+dfb = data.frame(x=x,yb = yb)
+dfc = data.frame(x=x, yc = yc)
+
+library(ggplot2)
+ga = ggplot(dfa, aes(x = x, y = ya, col=I("red"))) + geom_line(lwd = 10)
+gb = ggplot(dfb, aes(x = x, y = yb, col = I("blue") )) + geom_line(lwd = 10)
+gc = ggplot(dfc, aes(x=x,y=yc, col = I("green"))) + geom_line(lwd = 10)
+windows()
+ga
+gb
+gc
+
+# Ex2.2
+# 
+
+# What would be expected under 100 rolls of such a die?
+100*ya
+100*yb
+100*yc
