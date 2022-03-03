f1ece16f973cd2867548c0400b9365911494fb8c
Daniel-Carpenter <dbry1024@gmail.com>
2022-03-03 17:44:57 -0600

    Lab 5 Actual Submission (Cleanup)

--- 03 - Labs/Lab 05 - Monte Carlo Sim/OpenBUGS Models/Base SLR Model beta2 Squared/Jags-SLR-Model.R
+++ 03 - Labs/Lab 05 - Monte Carlo Sim/OpenBUGS Models/Base SLR Model beta2 Squared/Jags-SLR-Model.R
@@ -1,8 +1,3 @@
-# Jags-ExampleScript.R 
-# Accompanies the book:
-#   Kruschke, J. K. (2014). Doing Bayesian Data Analysis: 
-#   A Tutorial with R, JAGS, and Stan. 2nd Edition. Academic Press / Elsevier.
-
 # Optional generic preliminaries:
 graphics.off() # This closes all of R's graphics windows.
 # rm(list=ls())  # Careful! This clears all of R's memory!
@@ -11,7 +6,7 @@ graphics.off() # This closes all of R's graphics windows.
 source("DBDA2E-utilities.R") # Must be in R's current working directory.
 require(rjags)               # Must have previously installed package rjags.
 
-fileNameRoot="JAGS_Lab5_Task6_Ouput_Folder/" # For output file names.
+fileNameRoot="JAGS_Lab5_Task6.8_Ouput_Folder/" # For output file names.
 dir.create(fileNameRoot)
 
 # Load the data:
@@ -25,7 +20,7 @@ dataList = list(    # Put the information into a list.
   n = Ntotal 
 )
 
-# Define the model:
+# Define the model: (from pretty print OpenBUGS model)
 modelString = "
 model{
 	for( i in 1 : n ) {
@@ -41,11 +36,8 @@ model{
 writeLines( modelString , con="TEMPmodel.txt" )
 
 # Initialize the chains based on MLE of data.
-# Option: Use single initial value for all chains:
-#  thetaInit = sum(y)/length(y)
-#  initsList = list( theta=thetaInit )
 # Option: Use function that generates random values for each chain:
-initsList = list(beta0=0, beta1=0, sigma=10)#, beta2=0) # later
+initsList = list(beta0=0, beta1=0, sigma=10)
 
 # Run the chains:'
 jagsModel = jags.model( file="TEMPmodel.txt" , data=dataList , inits=initsList , 
@@ -54,24 +46,34 @@ jagsModel = jags.model( file="TEMPmodel.txt" , data=dataList , inits=initsList ,
 # Don't use the first 500 iterations
 update( jagsModel , n.iter=500 )
 
-codaSamples = coda.samples( jagsModel , variable.names=c("beta0", "beta1", "sigma"),#, "beta2") # for later
+codaSamples = coda.samples( jagsModel , variable.names=c("beta0", "beta1", "sigma"),
                             n.iter=33340 )
 save( codaSamples , file=paste0(fileNameRoot,"Mcmc.Rdata") )
 
-# Examine the chains:
-diagMCMC( codaObject = codaSamples, parName = c("beta0"))
-diagMCMC( codaObject = codaSamples, parName = c("beta1"))
-# diagMCMC( codaObject = codaSamples, parName = c("beta2"))
-diagMCMC( codaObject = codaSamples, parName = c("sigma"))
+# PLOTS =========================================================
 
+# Examine the chains -----------------------------------------------
+# Beta 0
+filename = paste0(fileNameRoot,"beta0_diagMCMC")
+diagMCMC( codaObject = codaSamples, parName = c("beta0"))
+saveGraph( file=filename , type="png" )
+
+# Beta 1
+filename = paste0(fileNameRoot,"beta1_diagMCMC")
+diagMCMC( codaObject = codaSamples, parName = c("beta1"))
+saveGraph( file=filename , type="png" )
+
+# Sigma
+filename = paste0(fileNameRoot,"sigma_diagMCMC")
+diagMCMC( codaObject = codaSamples, parName = c("sigma"))
+saveGraph( file=filename , type="png" )
+
+# Example the point Estimates --------------------------------------
 # Beta 0
 openGraph()
-filename = paste0(fileNameRoot,"beta0_Post")
 par( mar=c(3.5,0.5,2.5,0.5) , mgp=c(2.25,0.7,0) )
 plotPost( codaSamples[,"beta0"] , main="beta0" , xlab=bquote(beta[0]) )
-saveGraph( file= , type="png" )
-
-knitr::include_graphics(paste0(filename, "png"))
+saveGraph( file=filename , type="png" )
 
 # Beta 1
 openGraph()
@@ -85,11 +87,6 @@ par( mar=c(3.5,0.5,2.5,0.5) , mgp=c(2.25,0.7,0) )
 plotPost( codaSamples[,"sigma"] , main="sigma" , xlab=bquote(sigma) )
 saveGraph( file=paste0(fileNameRoot,"sigma_Post") , type="png" )
 
-# Beta 2
-# openGraph()
-# par( mar=c(3.5,0.5,2.5,0.5) , mgp=c(2.25,0.7,0) )
-# plotPost( codaSamples[,"beta2"] , main="beta2" , xlab=bquote(beta[2]) )
-# saveGraph( file=paste0(fileNameRoot,"beta2_Post") , type="png" )
 
-# Summary of all the models
+# Summary of all the models (Bayesian Point interval estimates)
 summary(codaSamples)
