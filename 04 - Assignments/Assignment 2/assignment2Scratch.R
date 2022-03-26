99a4d60e84eee9cfb770780e2d9786acba119cbe
Daniel Carpenter <59896063+Daniel-Carpenter@users.noreply.github.com>
2022-03-26 11:14:42 -0500

    Problem 4 - coded plots in scratch file

--- 04 - Assignments/Assignment 2/assignment2Scratch.R
+++ 04 - Assignments/Assignment 2/assignment2Scratch.R
@@ -52,7 +52,8 @@ Mymixplot <- function(w, a1,b1, a2,b2) {
 }
 Mymixplot(0.5, 10,2,3,50)
 
-# ==========================================================
+
+# TASK 2 ==========================================================
 
 Mymixbeta <- function(w=0.5, n=10, x, a1=1, a2=1, b1=1, b2=1) {
   # Specify the prior:
@@ -114,4 +115,110 @@ Mymixbeta <- function(w=0.5, n=10, x, a1=1, a2=1, b1=1, b2=1) {
 
 Mymixbeta(w=0.5, n=10, x=4, a1=2, a2=2, b1=4, b2=4)
 
+
+
+# TASK 3 =============================================================================
+
+
+mypriormix <- function() {
+  
+  # Optional generic preliminaries:
+  graphics.off() # This closes all of R's graphics windows.
+  rm(list=ls())  # Careful! This clears all of R's memory!
+  
+  
+  # Load the functions used below:
+  source("DBDA2E-utilities.R") # Must be in R's current working directory.
+  require(rjags)               # Must have previously installed package rjags.
+  
+  fileNameRoot="Assn2Jags/" # For output file names.
+  dir.create(fileNameRoot)
+  
+  # Load the data:
+  Ntotal = 10  # Compute the total number of flips.
+  dataList = list(x = 15, n = 20)
+  
+  # Define the model:
+  modelString = "
+  model {
+    x ~ dbin( p, n ) p <- theta[pick] 
+    
+    # pick is 2 if biased 1 unbiased 
+    pick ~ dcat(q[]) # categorical 1 produced prob q[1], etc 
+    
+    q[1]<-0.9 
+    q[2]<-0.1 
+    theta[1] <-0.5        # unbiased 
+    theta[2] ~ dunif(0,1) # biased
+    biased <- pick - 1
+  }
+  " # close quote for modelString
+  writeLines( modelString , con="TEMPmodel.txt" )
+  
+  # Initialize the chains based on MLE of data.
+  initsList = list( pick = 1)
+  
+  # Run the chains:
+  jagsModel = jags.model( file="TEMPmodel.txt" , data=dataList , inits=initsList , 
+                          n.chains=3 , n.adapt=500 )
+  update( jagsModel , n.iter=500 )
+  codaSamples = coda.samples( jagsModel , variable.names=c("theta[2]","biased","theta[1]") ,
+                              n.iter=3334 )
+  save( codaSamples , file=paste0(fileNameRoot,"Mcmc.Rdata") )
+  
+  # Examine the chains ----------------------------------------------------------
+  
+  # Convergence diagnostics:
+  filename1 <- paste0(fileNameRoot,"Theta[2]Diag")
+  diagMCMC( codaObject=codaSamples , parName="theta[2]" )
+  saveGraph( file= filename1, type="jpg" )
+  
+  
+  # Posterior descriptives:
+  filename2 <- paste0(fileNameRoot,"Theta[2]Post")
+  openGraph(height=3,width=4)
+  par( mar=c(3.5,0.5,2.5,0.5) , mgp=c(2.25,0.7,0) )
+  plotPost( codaSamples[,"theta[2]"] , main="theta[2]" , xlab=bquote(theta[2]) )
+  saveGraph( file=filename2, type="jpg" )
+  graphics.off() # this is done to prevent very last graph being displyed in RMD out
+  
+  
+  # Summary Statistics/Point Estimates
+  return(list(su = summary(codaSamples), codaSamples = codaSamples,
+              diagGraph = paste0(filename1, '.jpg'), postGraph = paste0(filename2, '.jpg')))
+}
+
+mypriormix() -> ans
+
+
+
+# TASK 4 =======================================================================
+
+class(ans$codaSamples) # Class is a list
+str(ans$codaSamples)   # Structure of the mcmc list
+
+# Use codaSamples to get the Monitored Variables - show head too
+mcmc1 = as.data.frame(ans$codaSamples[[1]]); head(mcmc1)
+mcmc2 = as.data.frame(ans$codaSamples[[2]]); head(mcmc2)
+mcmc3 = as.data.frame(ans$codaSamples[[3]]); head(mcmc3)
+
+# make mcmcT by combining above
+mcmcT = rbind.data.frame(mcmc1,mcmc2,mcmc3)
+
+require(ggplot2)
+
+# Problem c
+ggplot(mcmcT, aes(x = `theta[2]`, fill = biased)) +
+  geom_histogram() + # Create a  hist layer
+  facet_wrap(~biased) +
+  labs(title = 'Theta[2] Seperated by Biased and Unbiased',
+  subtitle = 'Daniel Carpenter') +
+  theme_minimal() + theme(text = element_text(color = '#666666'))
+  
+# Problem e
+ggplot(mcmcT, aes(x = `theta[2]`)) +
+  geom_histogram(color = 'skyblue4', fill = 'skyblue', alpha = 0.5) + # Create a  hist layer
+  labs(title = 'Incorrect Posterior from MCMC Sample',
+  subtitle = 'Theta[2] MCMC Sample: Gibbs Updating from a Mixture - Daniel Carpenter') +
+  theme_minimal() + theme(text = element_text(color = '#666666'))
 
