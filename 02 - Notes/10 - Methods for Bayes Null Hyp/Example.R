0df7f9323e5081e3c548887d8e4addddb9818d2a
Daniel-Carpenter <dbry1024@gmail.com>
2022-04-03 11:49:28 -0500

    Notes Initial Commit

---
+++ 02 - Notes/10 - Methods for Bayes Null Hyp/Example.R
@@ -0,0 +1,34 @@
+# ch12
+# compare two sample t-tests with Bayesian equivalent
+# 
+
+mycomp = function(mu1=15,mu2=20,sd1 = 5, sd2 =10, n1=10,n2=10,iter = 10, alpha = 0.05)
+{
+  if(!require(BEST)) install.packages(BEST) # check for BEST package
+  data1 = rnorm(iter*n1,mean = mu1, sd = sd1)
+  data2 = rnorm(iter*n2, mean = mu2, sd = sd2)
+  
+  mat1 = matrix(data1, nr = n1, nc = iter, byrow = TRUE)
+  mat2 = matrix(data2, nr = n2, nc = iter, byrow = TRUE)
+  
+  matout = matrix(NA, nr=iter, nc = 4, dimnames=list(1:iter,c("BL","BU", "CL","CU")))
+  
+  for (i in 1:iter)
+  {
+    bo=BESTmcmc(mat1[,i],mat2[,i])
+   
+   muDiff <- bo$mu1 - bo$mu2
+   matout[i,1]<- quantile(muDiff,alpha/2)
+   matout[i,2]<- quantile(muDiff,1-alpha/2)
+   tto=t.test(mat1[,i], mat2[,i],var.equal = FALSE) 
+   matout[i,3] = tto$conf.int[1]
+   matout[i,4] = tto$conf.int[2]
+  }
+  dev.new(noRStudioGD = TRUE)
+  matplot(matout)
+  list(matout = matout)
+}
+
+mycomp(iter = 2)
+
+
